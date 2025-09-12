7eec94d9b387fd16a669de7c3c6624c2ae5b816a
leaf48 <watavita2004@gmail.com>
2025-09-11 23:43:15 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use log::info;
 use Twitch_AI_Chatbot::{
     config::CONFIG,
     logger::LoggerSetup,
-    twitch::{Twitch, UserMessagePayload},
     workflows::{model::MessagePayload, send_msg::send_msg},
 };
 
