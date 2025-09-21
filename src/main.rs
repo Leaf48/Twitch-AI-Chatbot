0c0f825e4d5e267796a4659b55d33b892542417f
leaf48 <watavita2004@gmail.com>
2025-09-21 02:02:17 -0700

    feat: main job flow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use log::info;
+use std::time::Duration;
+
+use log::{debug, info, warn};
+
+use tokio::time::{sleep, timeout};
 use Twitch_AI_Chatbot::{
-    config::CONFIG,
+    config::{
+        channel::{can_send_now, init_channels},
+        CONFIG,
+    },
     logger::LoggerSetup,
-    workflows::{model::MessagePayload, send_msg::send_msg},
+    workflows::recv_and_send_msg::recv_and_send_msg,
 };
 
 #[tokio::main]
@@ -10,9 +17,26 @@ async fn main() {
     LoggerSetup::new();
     info!("Available chatbots: {:?}", CONFIG.accounts.len());
 
-    send_msg(MessagePayload {
-        account: CONFIG.accounts[0].clone(),
-        text: "Hello World".to_string(),
-    })
-    .await;
+    init_channels();
+
+    loop {
+        for account in &CONFIG.accounts {
+            // check if interval time elapsed
+            if !can_send_now(account) {
+                continue;
+            }
+
+            // timeout if it exeeds set time.
+            match timeout(
+                Duration::from_secs(account.timeout.try_into().unwrap()),
+                recv_and_send_msg(account),
+            )
+            .await
+            {
+                Ok(()) => debug!("completed: {}", account.account_name),
+                Err(_) => warn!("timeout: {} secs", account.timeout),
+            }
+        }
+        sleep(Duration::from_secs(1)).await;
+    }
 }
